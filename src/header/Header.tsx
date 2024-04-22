84497d5d80f0df3051609be1aff48df28cadd47a
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-04-23 02:51:06 +0300

    feat: updated header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,10 +1,15 @@
 import styles from './Header.module.scss'
 
+import logoImage from '../assets/images/logo.webp'
+
 export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <div>BrainShock</div>
+        <div className={styles.logo}>
+          <img alt={'Logo'} src={logoImage} />
+          <span>BrainShock</span>
+        </div>
       </div>
     </header>
   )
