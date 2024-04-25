7cd2c4e30bbea686c0bd7656fa03295f73c032e5
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-04-26 01:46:10 +0300

    feat: added bg video

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import Iframe from 'react-iframe'
+
 import { Header } from '@/header/Header'
 
+import styles from './App.module.scss'
+
 export function App() {
+  const bgVideoUrl =
+    'https://www.youtube.com/embed/aCPGTNoizxw?autoplay=1&mute=1&controls=0&loop=1&playlist=aCPGTNoizxw&vq=hd1080'
+
   return (
     <div>
+      <div className={styles.pageBackground}>
+        <Iframe
+          allow={
+            'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          }
+          allowFullScreen
+          className={styles.bgVideo}
+          display={'block'}
+          frameBorder={0}
+          id={''}
+          position={'relative'}
+          referrerpolicy={'strict-origin-when-cross-origin'}
+          url={bgVideoUrl}
+        />
+      </div>
+
       <Header />
     </div>
   )
