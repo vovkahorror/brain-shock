514497c7935e8d1114e220ecc2f0b36eec76e00b
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-04-25 05:03:31 +0300

    feat: updated header

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -4,6 +4,8 @@ import logoImage from '../assets/images/logo.webp'
 import TelegramIcon from '../assets/images/telegram.svg?react'
 
 export const Header = () => {
+  const telegramLink = 'https://t.me/Brain_Shock'
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -12,7 +14,14 @@ export const Header = () => {
           <span>BrainShock</span>
         </div>
         <div>
-          <TelegramIcon />
+          <a className={styles.link} href={telegramLink} rel={'noreferrer'} target={'_blank'}>
+            <span className={styles.iconWrapperTop}>
+              <TelegramIcon className={styles.icon} />
+            </span>
+            <span className={styles.iconWrapperBottom}>
+              <TelegramIcon className={styles.icon} />
+            </span>
+          </a>
         </div>
       </div>
     </header>
