4de9c90261db2ed2ea8e4f614048a84187e9635e
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-04-27 03:54:20 +0300

    feat: updated bg video

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import Iframe from 'react-iframe'
+import ReactPlayer from 'react-player'
 
 import { Header } from '@/header/Header'
 
 import styles from './App.module.scss'
 
 export function App() {
-  const bgVideoUrl =
-    'https://www.youtube.com/embed/aCPGTNoizxw?autoplay=1&mute=1&controls=0&loop=1&playlist=aCPGTNoizxw&vq=hd1080'
+  const bgVideoUrl = 'https://https://www.youtube.com/embed/aCPGTNoizxw'
 
   return (
     <div>
       <div className={styles.pageBackground}>
-        <Iframe
-          allow={
-            'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-          }
-          allowFullScreen
+        <ReactPlayer
           className={styles.bgVideo}
-          display={'block'}
-          frameBorder={0}
-          id={''}
-          position={'relative'}
-          referrerpolicy={'strict-origin-when-cross-origin'}
+          height={1080}
+          loop
+          muted
+          playing
           url={bgVideoUrl}
+          width={1920}
         />
       </div>
 
