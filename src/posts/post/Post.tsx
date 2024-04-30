495fc4cf4138d5076f383f30cc286d858be1bf25
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-05-01 01:07:19 +0300

    fix: W3C validation

--- src/posts/post/Post.tsx
+++ src/posts/post/Post.tsx
@@ -9,7 +9,9 @@ export const Post = ({ post }: PostProps) => {
     <article className={styles.post}>
       <div className={styles.image} style={{ backgroundImage: `url(${photos[0]})` }}></div>
       <div>
-        <h1 className={styles.title}>{title}</h1>
+        <h2>
+          <span className={styles.title}>{title}</span>
+        </h2>
         <span className={styles.price}>{price} грн</span>
         <span className={styles.condition}>стан: {condition}</span>
         <p className={styles.description}>{description}</p>
