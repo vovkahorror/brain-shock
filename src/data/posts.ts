45cfe217cd0c8552f84c5a0ecee4441b79f65900
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-05-16 00:54:29 +0300

    feat: added posts

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,4 +1,4 @@
-import { descriptionNew } from '@/common/consts/descriptions'
+import { descriptionNew, descriptionUsed } from '@/common/consts/descriptions'
 
 import new_lite_01 from '../assets/images/consoles/new/lite/new_lite_01.webp'
 import new_lite_02 from '../assets/images/consoles/new/lite/new_lite_02.webp'
@@ -23,6 +23,16 @@ import new_v2_05 from '../assets/images/consoles/new/v2/new_v2_05.webp'
 import new_v2_06 from '../assets/images/consoles/new/v2/new_v2_06.webp'
 import new_v2_07 from '../assets/images/consoles/new/v2/new_v2_07.webp'
 import new_v2_08 from '../assets/images/consoles/new/v2/new_v2_08.webp'
+import used_oled_white_01 from '../assets/images/consoles/used/oled/white/used_oled_white_01.webp'
+import used_oled_white_02 from '../assets/images/consoles/used/oled/white/used_oled_white_02.webp'
+import used_oled_white_03 from '../assets/images/consoles/used/oled/white/used_oled_white_03.webp'
+import used_oled_white_04 from '../assets/images/consoles/used/oled/white/used_oled_white_04.webp'
+import used_oled_white_05 from '../assets/images/consoles/used/oled/white/used_oled_white_05.webp'
+import used_oled_white_06 from '../assets/images/consoles/used/oled/white/used_oled_white_06.webp'
+import used_oled_white_07 from '../assets/images/consoles/used/oled/white/used_oled_white_07.webp'
+import used_oled_white_08 from '../assets/images/consoles/used/oled/white/used_oled_white_08.webp'
+import used_oled_white_09 from '../assets/images/consoles/used/oled/white/used_oled_white_09.webp'
+import used_oled_white_10 from '../assets/images/consoles/used/oled/white/used_oled_white_10.webp'
 
 export const posts: PostsDataType = {
   new: [
@@ -78,7 +88,29 @@ export const posts: PostsDataType = {
       title: 'Nintendo Switch Lite',
     },
   ],
-  used: [],
+  used: [
+    {
+      color: 'white',
+      condition: 'вживаний',
+      description:
+        'Консоль вживана має гарний косметичний стан, консоль немає дефектів чи якихось проблем. З переду ідеал, на екрані без подряпин. На задній кришці є легкі потертості.\n' +
+        descriptionUsed,
+      photos: [
+        used_oled_white_01,
+        used_oled_white_02,
+        used_oled_white_03,
+        used_oled_white_04,
+        used_oled_white_05,
+        used_oled_white_06,
+        used_oled_white_07,
+        used_oled_white_08,
+        used_oled_white_09,
+        used_oled_white_10,
+      ],
+      price: 13999,
+      title: 'Nintendo Switch OLED White',
+    },
+  ],
 }
 
 export type PostsDataType = {
@@ -87,6 +119,7 @@ export type PostsDataType = {
 }
 
 export type PostType = {
+  color?: string
   condition: 'вживаний' | 'новий'
   description: string
   photos: string[]
