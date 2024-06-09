ec1996548f9bc6319755b5efe3f2e40c13fe8745
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-06-10 01:26:17 +0300

    feat: updated pre rendering

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,10 @@ import * as path from 'path'
 
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import vitePrerender from 'vite-plugin-prerender'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    vitePrerender({
-      routes: ['/', '/new/*', '/used/*'],
-      staticDir: path.join(__dirname, 'dist'),
-    }),
-  ],
+  plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
