8e23a347b3a9bc70b8a7fc83ef62760f7e8dd40d
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-07-04 10:46:00 +0300

    feat: updated sitemap

--- src/generate-sitemap.ts
+++ src/generate-sitemap.ts
@@ -1,9 +1,13 @@
 import fs from 'fs'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
 import { siteUrl } from './common/consts/links'
 import { formatStringToUrlFormat } from './common/helpers/formatStringToUrlFormat'
 import { postsData } from './data/posts-data'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export const generateSitemap = () => {
   const urls = []
