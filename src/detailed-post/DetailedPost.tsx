8cd471e7237323772ef1674bfd787096600b3cf1
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-07-15 02:01:33 +0300

    feat: updated schema.org

--- src/detailed-post/DetailedPost.tsx
+++ src/detailed-post/DetailedPost.tsx
@@ -101,7 +101,7 @@ const DetailedPost = memo(() => {
           businessDays: '1-2',
         },
         shippingRate: {
-          '@type': 'PropertyValue',
+          '@type': 'MonetaryAmount',
           currency: 'UAH',
           value: true,
         },
