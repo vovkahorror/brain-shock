358f7047edccbd5d5840df9046d97bb00c41fba1
Volodymyr Yaremchak <vovkahorror@gmail.com>
2024-08-14 00:27:16 +0300

    feat: updated hasMerchantReturnPolicy

--- src/common/consts/calculations.ts
+++ src/common/consts/calculations.ts
@@ -54,7 +54,7 @@ export const shippingDetails = {
 
 export const hasMerchantReturnPolicy = {
   '@type': 'MerchantReturnPolicy',
-  applicableCountry: 'UA',
+  applicableCountry: 'UKR',
   merchantReturnDays: 7,
   name: 'Політика повернення',
   returnFees: 'ReturnShippingFees',
